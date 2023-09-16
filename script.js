96e76f14c8b0d24131b93c8d924f4858fa063240
GhostMobin <s1810677110@ru.ac.bd>
2023-09-16 12:56:34 +0600

    testimonial contact

---
+++ script.js
@@ -0,0 +1,9 @@
+const accordionItems = document.querySelectorAll(".accordion-item");
+
+accordionItems.forEach(item => {
+    const header = item.querySelector(".accordion-header");
+
+    header.addEventListener("click", () => {
+        item.classList.toggle("active");
+    })
+})
